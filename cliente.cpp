7d3bc1f160a23fe547e9de3b6526341c1bfb38bd
aleax888 <42581086+aleax888@users.noreply.github.com>
2022-06-12 18:39:24 -0500

    Add files via upload

--- cliente.cpp
+++ cliente.cpp
@@ -1,137 +1,173 @@
 #include <iostream>
 #include <string>
-#include <string.h>
 #include <vector>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
 using namespace std;
 
-void createProtocol(string comando, char* buffer, int& tamProtocolo);
-
-int main(){
-    
-    char buffer[1000];
-    
-    struct sockaddr_in stSockAddr;
-    int Res;
-    int SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-    int n;
-
-    if (-1 == SocketFD)
-    {
-      perror("cannot create socket");
-      exit(EXIT_FAILURE);
-    }
- 
-    memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
- 
-    stSockAddr.sin_family = AF_INET;
-    stSockAddr.sin_port = htons(45081);
-    Res = inet_pton(AF_INET, "5.253.235.219", &stSockAddr.sin_addr);
- 
-    if (0 > Res)
-    {
-      perror("error: first parameter is not a valid address family");
-      close(SocketFD);
-      exit(EXIT_FAILURE);
-    }
-    else if (0 == Res)
-    {
-      perror("char string (second parameter does not contain valid ipaddress");
-      close(SocketFD);
-      exit(EXIT_FAILURE);
-    }
-
-    if (-1 == connect(SocketFD, (const struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in)))
-    {
-      perror("connect failed");
-      close(SocketFD);
-      exit(EXIT_FAILURE);
-    }
-
-    while (1)
-    {
-
-        string comando;
-        int tamProtocolo = 0;
-        cout << "> GDBD ";
-        getline(cin, comando);
-
-        cout << "Comnado Ingresado: " << comando <<endl;
-
-        createProtocol(comando, buffer, tamProtocolo);
-        cout << "Buffer: "<< buffer << endl;
-        cout << "Tamaño: "<< tamProtocolo << endl;
-
-        n = write(SocketFD, buffer, tamProtocolo);
-        
-        // n = read(SocketFD,buffer,1000);
-
-        cout << buffer;
-
-        memset(buffer, 0, 1000);
-    }
-    
+string number_format(int number)
+{
+    string ans = to_string(number);
+    int aux = ans.size();
+    if (aux < 3)
+        ans.insert(ans.begin(), 3 - aux, '0');
+    return ans;
 }
 
-void createProtocol(string comando, char* buffer, int& tamProtocolo){
-    string protocol;
-    char aux[1000];
-    char type = comando[0];
+string make_protocol_C1(string nombre_del_nodo)
+{
+    string ans = "C1" + number_format(nombre_del_nodo.size()) + nombre_del_nodo;
+    return ans;
+}
 
-    // Contar cantidad de espacios
-    int cont = 0;
-    for(size_t i = 0; i < comando.size(); i++){
-        if(comando[i] == ' '){
-            cont++;
-        }
+string make_protocol_C2(string nombre_del_nodo1, string nombre_de_la_relacion, string nombre_del_nodo2)
+{
+    string ans = "C2" + 
+        number_format(nombre_del_nodo1.size()) + nombre_del_nodo1 +
+        number_format(nombre_de_la_relacion.size()) + nombre_de_la_relacion +
+        number_format(nombre_del_nodo2.size()) + nombre_del_nodo2;
+    return ans;
+}
+
+string make_protocol_D1(string nombre_de_la_relacion)
+{
+    string ans = "D1" + number_format(nombre_de_la_relacion.size()) + nombre_de_la_relacion;
+    return ans;
+}
+
+string make_protocol_D2(string nombre_del_nodo1, string nombre_de_la_relacion, string nombre_del_nodo2)
+{
+    string ans = "D2" +
+        number_format(nombre_del_nodo1.size()) + nombre_del_nodo1 +
+        number_format(nombre_de_la_relacion.size()) + nombre_de_la_relacion +
+        number_format(nombre_del_nodo2.size()) + nombre_del_nodo2;
+    return ans;
+}
+
+string make_protocol_D3(string nombre_del_nodo, string nombre_del_atributo)
+{
+    string ans = "D3" + 
+        number_format(nombre_del_nodo.size()) + nombre_del_nodo +
+        number_format(nombre_del_atributo.size()) + nombre_del_atributo;
+    return ans;
+}
+
+int interpret_protocol_A(string protocolA)
+{
+    cout << protocolA.substr(5, protocolA.size() - 1);
+    return protocolA[1];
+}
+
+void space_format(string &str)
+{
+    str.push_back(' ');
+    for (int i = str.find(' '), s = str.size(); i < s and i > -1; i = str.find(' ', i))
+    {
+        int space_begin = i, count = 0;
+        for (; i < s and i > -1 and str[i] == ' '; count++, i++);
+        
+        if (count > 0) 
+            str.replace(space_begin, count, " ");
+        i = space_begin + 1;
     }
+}
 
-    // Identificar tipo de comando
-    if(type == 'C'){
-        if(cont == 1){
-            string type = "C1";
-            size_t nodePos = comando.find(" ");
-            size_t nodeLen = comando.size() - ((nodePos)+1);
-            sprintf(aux, "%03d", nodeLen);
-            aux[4] = '\0';
-            string len = aux;
-            string node = comando.substr( nodePos + 1, nodeLen);
-            
-            protocol = type + len + node;
+int count_spaces(string str)
+{
+    int ans = 0;
 
-        }
-        if(cont == 3){
-            comando.append(" ");
-            string type = "C2";
-            size_t nodePos = comando.find(" ");
-            size_t auxPos;
+    for (int i = str.find(' '), s = str.size(); i < s and i > -1; i = str.find(' ', i + 1), ans++);
+    
+    return ans;
+}
 
-            for (int i = 0; i < cont; i++){
-                auxPos = comando.find(" ", nodePos+1, 1);
-                size_t nodeLen = auxPos - (nodePos + 1);
-                sprintf(aux, "%03d", nodeLen);
-                aux[4] = '\0';
-                string len = aux;
-                type.append(len);
-                type.append(comando.substr(nodePos + 1, nodeLen));
-
-                nodePos = comando.find(" ", nodePos+1, 1);
-            }
-            protocol = type;
-        }
+void split_query(vector<string>&ans, string query)
+{
+    while (!query.empty())
+    {
+        ans.push_back(query.substr(0, query.find(' ')));
+        query.replace(0, query.find(' ') + 1, "");
     }
+}
 
-    for(size_t i = 0; i < protocol.size(); i++){
-        *(buffer + i)= protocol[i];
+vector<string> interpret_query(string query)
+{
+    vector<string> query_parts;
+    query_parts.push_back(query.substr(5, 1));
+    split_query(query_parts, query.substr(7, query.size() - 1));
+
+    /*cout << "parsing: " << endl;
+    for (int i = 0, s = query_parts.size(); i < s; ++i)
+    {
+        cout << query_parts[i] << endl;
+    }*/
+
+    return query_parts;
+}
+
+
+
+void menu()
+{
+    string aux = "GDBD ";
+    string packet;
+    
+    while (true)
+    {
+        cout << aux;
+        string query; getline(cin, query);
+        query = aux + query + ' ';
+        space_format(query);
+        vector<string> query_parts = interpret_query(query);
+
+        /*cout << "parsing: " << endl;
+        for (int i = 0, s = query_parts.size(); i < s; ++i)
+        {
+            cout << query_parts[i] << endl;
+        }*/
+
+        if (query_parts[0] == "C")
+        {
+            int count = count_spaces(query);
+            if (count == 3)
+                packet = make_protocol_C1(query_parts[1]);
+            else if (count == 5)
+                packet = make_protocol_C2(query_parts[1], query_parts[2], query_parts[3]);
+
+            // wait protocol A
+        }
+
+        else if (query_parts[0] == "R")
+        {
+            // working progress
+        }
+
+        else if (query_parts[0] == "U")
+        {
+            // working progress
+        }
+
+        else if (query_parts[0] == "D")
+        {
+            int count = count_spaces(query);
+            if (count == 3)
+                packet = make_protocol_D1(query_parts[1]);
+            else if (count == 5)
+                packet = make_protocol_D2(query_parts[1], query_parts[2], query_parts[3]);
+            else if (count == 4)
+                packet = make_protocol_D3(query_parts[1], query_parts[2]);
+
+            // wait protocol A
+        }
+
+        else
+        {
+            cout << "error" << endl;
+        }
+
+        cout << "packet: " << packet << endl << endl;
     }
-    buffer[protocol.size()] = '\0';
+}
 
-    tamProtocolo = protocol.size()+1;
+int main()
+{ 
+    menu();
 }
